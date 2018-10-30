37ae1da37e2568e0087f22777c6e4c677d402cf2
Davide Bulbarelli <private@dadebulba.com>
2018-10-30 16:50:18 +0100

    Hello world

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
-hello 
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+var greeting = { hello : "Hello world"};
+
+app.get('/', (req, res) => res.send('Hello World!'))
+ 
+app.get('/greeting', (req, res) => {
+   res.json(greeting)
+})
+ 
+app.listen(PORT, () => console.log('ProgIng2 app listening on port'+ PORT))
+
