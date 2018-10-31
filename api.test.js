918d483c5beb282455a89fd485d311386398d64f
Davide Bulbarelli <private@dadebulba.com>
2018-10-31 11:57:03 +0100

    Fixed test

--- api.test.js
+++ api.test.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 
 it('works with promise', () => {
-    expect.assertions(2); // mi aspetto che ci siano 2 expect dopo, utile per una catena di promise
-    fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400").then(
+    expect.assertions(1); // mi aspetto che ci siano 2 expect dopo, utile per una catena di promise
+    return fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400").then(
         res => {expect(res.status).toBe(200)}
     )
 })
