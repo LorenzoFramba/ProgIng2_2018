54193daac403f34701c705c787e293e90061a31f
morgan <morgan.malavsi@gmail.com>
2018-12-09 22:03:43 +0100

    start testing task

--- test/task/task.test.js
+++ test/task/task.test.js
@@ -1,5 +1,3 @@
-const taskGET = require('../api/task');
-const taskPUT = require('../api/task');
-const taskDELETE = require('../api/task');
-const taskPOST = require('../api/task');
+const fetch = require("node-fetch");
+let Task = require("../../model/task");
 
