d4f2737694248bb129826e3c03261248405d1e13
morgan <morgan.malavsi@gmail.com>
2018-12-10 09:47:25 +0100

    miei salvataggi

--- test/task/task.test.js
+++ test/task/task.test.js
@@ -1,3 +1,5 @@
 const fetch = require("node-fetch");
 let Task = require("../../model/task");
 
+const errors = require('../../api/errorMsg');
+const USER_ENDPOINT = "http://localhost:3000/v1/Tasks/";
